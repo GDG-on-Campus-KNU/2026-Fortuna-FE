d729ebc6d5e34b0973aaeddd8082a368d6946e14
Huigyun Jeong <topone03@gmail.com>
2026-06-01 15:29:13 +0900

    [#15] fix: remove redundant translateY reset when navigating back in AudioPlayerScreen

--- src/screens/AudioPlayerScreen.tsx
+++ src/screens/AudioPlayerScreen.tsx
@@ -65,7 +65,6 @@ export default function AudioPlayerScreen() {
             useNativeDriver: true,
           }).start(() => {
             router.back();
-            translateY.setValue(0);
           });
         } else {
           Animated.spring(translateY, {
